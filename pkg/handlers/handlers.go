68ca2296a2bbc2925502a9b5b296b1efb9476c38
Sanjar <sanjarbakhramovich@gmail.com>
2023-10-03 23:07:19 +0500

    add new templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,15 +1,16 @@
-package main
+package handlers
 
 import (
+	"myGoWebApp/pkg/render"
 	"net/http"
 )
 
 // Home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
