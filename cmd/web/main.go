33761670fd6698f8dd15ce09bf5d6104a23e6520
Sanjar <sanjarbakhramovich@gmail.com>
2023-10-04 15:09:59 +0500

    build a simple template cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,6 @@ func main() {
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
-	fmt.Println(fmt.Sprintf("Starting server at port %s", portNumber))
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
 }
